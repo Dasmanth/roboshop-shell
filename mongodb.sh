2865a390e8502efcca7c6afcf5d95aaa6360d3cf
Dasmanth <dasmanthkare@gmail.com>
2023-09-04 08:46:53 +0530

    roboshop ngnix script

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,9 @@
 echo -e "\e[33mcopy mongodb repo file\e[0m"
 cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+
 echo -e "\e[33minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
+
 echo -e "\e[33mstart mongodb server\e[0m"
 systemctl start mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
