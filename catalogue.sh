d9dd985f65de28e64b554327c899d64ca408030e
Dasmanth <dasmanthkare@gmail.com>
2023-09-12 15:26:45 +0530

    Catalogue service code

--- catalogue.sh
+++ catalogue.sh
@@ -5,13 +5,13 @@ echo -e "\e[33m installation of nodejs \e[0m"
 dnf install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[32m creaion of user \e[0m"
-useradd roboshop &>>/tmp/roboshop/log
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[32m creation of app directory \e[0m"
-mkdir /app &>>/tmp/roboshop/log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[32m downlaod catalogue content \e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop/log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app
 echo -e "\e[34m extract the content \e[0m"
 unzip /tmp/catalogue.zip
@@ -20,9 +20,9 @@ echo -e "\e[33m install npm \e[0m"
 npm install
 
 echo -e "\e[32m starting of catalogue service \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop/log
-systemctl enable catalogue &>>/tmp/roboshop/log
-systemctl start catalogue &>>/tmp/roboshop/log
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl start catalogue &>>/tmp/roboshop.log
 
 echo -e "\e[32m install mongodb \e[0m"
-dnf install mongodb-org-shell -y &>>/tmp/roboshop/log
+dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
