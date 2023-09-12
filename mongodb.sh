387a257f60c69840dc0a63a975df1c193b08db21
Dasmanth <dasmanthkare@gmail.com>
2023-09-12 15:25:20 +0530

    Catalogue service code

--- mongodb.sh
+++ mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[33minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mUpdate mongodb listen address\e[0m"
-sed -i -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf
+sed -i 's/127.0.0.0/0.0.0.0' /etc/mongod.conf &>>/tmp/roboshop.conf
 
 echo -e "\e[33mstart mongodb server\e[0m"
 systemctl start mongod &>>/tmp/roboshop.log
