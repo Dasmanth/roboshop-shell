11994db00a993843797e85a4b221c1a88f2195ac
Dasmanth <dasmanthkare@gmail.com>
2023-09-12 15:04:27 +0530

    Catalogue service code

---
+++ catalogue.sh
@@ -0,0 +1,27 @@
+echo -e "\e[32m setup node source \e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+
+echo -e "\e[33m installation of nodejs \e[0m"
+dnf install nodejs -y &>>/tmp/roboshop.log
+
+echo -e "\e[32m creaion of user \e[0m"
+useradd roboshop &>>/tmp/roboshop/log
+
+echo -e "\e[32m creation of app directory \e[0m"
+mkdir /app &>>/tmp/roboshop/log
+
+echo -e "\e[32m downlaod catalogue content \e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop/log
+cd /app
+echo -e "\e[34m extract the content \e[0m"
+unzip /tmp/catalogue.zip
+cd /app
+echo -e "\e[33m install npm \e[0m"
+npm install
+echo -e "\e[32m starting of catalogue service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop/log
+systemctl enable catalogue &>>/tmp/roboshop/log
+systemctl start catalogue &>>/tmp/roboshop/log
+
+echo -e "\e[32m install mongodb \e[0m"
+dnf install mongodb-org-shell -y &>>/tmp/roboshop/log
