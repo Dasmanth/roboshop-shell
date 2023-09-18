cdb4d4357fc5c948788276386a52c5c832b32bd4
Dasmanth <dasmanthkare@gmail.com>
2023-09-18 10:19:05 +0530

    Catalogue service modifications

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mcopy mongodb repo file\e[0m"
-cp mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
+cp /home/centos/robosho-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33minstalling mongodb server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
