e1a742d99cf42f190f11968f88d307afbdc3fa56
Dasmanth <dasmanthkare@gmail.com>
2023-10-02 08:32:59 +0530

    Common variables cart

--- catalogue.sh
+++ catalogue.sh
@@ -2,12 +2,5 @@ source common.sh
 component=catalogue
 nodejs
 
-echo -e "${color} copy mongoDb repo file ${nocolor}"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
-
-echo -e "${colors} Install mongoDb client ${nocolor}"
-yum install mongodb-org-shell -y &>>${log_file}
-
-echo -e "${color} Load schema ${nocolor}"
-mongo --host 172.31.26.22 <${app-path}/schema/$component.js &>>${log_file}
+mongo_schema_setup
 
--- common.sh
+++ common.sh
@@ -39,3 +39,15 @@ nodejs() {
  systemctl start $component &>>${log_file}
 
 }
+
+mongo_schema_setup () {
+
+  echo -e "${color} copy mongoDb repo file ${nocolor}"
+  cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log_file}
+
+  echo -e "${colors} Install mongoDb client ${nocolor}"
+  yum install mongodb-org-shell -y &>>${log_file}
+
+  echo -e "${color} Load schema ${nocolor}"
+  mongo --host 172.31.26.22 <${app-path}/schema/$component.js &>>${log_file}
+}
