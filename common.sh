adcb3f2c9a729aa1c2ea538b5852047e6f4409df
Dasmanth <dasmanthkare@gmail.com>
2023-10-02 09:24:37 +0530

    Common variables cart

--- common.sh
+++ common.sh
@@ -4,6 +4,32 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
+app_presetup() {
+   echo -e "${color} add application user ${nocolor}"
+   useradd roboshop &>>${log_file}
+
+   echo -e "${colors} Create application directory ${nocolor}"
+   rm -rf ${app_path} &>>${log_file}
+   mkdir ${app_path}
+
+   echo -e "${color} downlaod application content ${nocolor}"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+    cd ${app_path}
+
+   echo -e "${colors} extract application content ${nocolor}"
+   unzip /tmp/${component}.zip &>>${log_file}
+   cd ${app_path}
+
+}
+
+systemd_setup() {
+
+  echo -e "${color} start ${component} service ${nocolor}"
+   systemctl daemon-reload &>>${log_file}
+   systemctl enable ${component} &>>${log_file}
+   systemctl start ${component} &>>${log_file}
+}
+
 nodejs() {
 
  echo -e "${color} configuring nodejs repos ${nocolor}"
@@ -12,31 +38,16 @@ nodejs() {
  echo -e "${colors} installation of nodejs ${nocolor}"
  yum  install nodejs -y &>>${log_file}
 
- echo -e "${color} add application user ${nocolor}"
- useradd roboshop &>>${log_file}
+ app_presetup
 
- echo -e "${colors} Create application directory ${nocolor}"
- rm -rf ${app_path} &>>${log_file}
- mkdir ${app_path}
-
- echo -e "${color} downlaod application content ${nocolor}"
- curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>>${log_file}
- cd ${app_path}
-
- echo -e "${colors} extract application content ${nocolor}"
- unzip /tmp/$component.zip &>>${log_file}
- cd ${app_path}
 
  echo -e "${colors} install nodejs dependencies ${nocolor}"
  npm install &>>${log_file}
 
  echo -e "${color} setup systemd service${nocolor}"
- cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>>${log_file}
+ cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
- echo -e "${color} start $component service ${nocolor}"
- systemctl daemon-reload &>>${log_file}
- systemctl enable $component &>>${log_file}
- systemctl start $component &>>${log_file}
+ systemd_setup
 
 }
 
@@ -49,5 +60,30 @@ mongo_schema_setup () {
   yum install mongodb-org-shell -y &>>${log_file}
 
   echo -e "${color} Load schema ${nocolor}"
-  mongo --host 172.31.26.22 <${app-path}/schema/$component.js &>>${log_file}
+  mongo --host 172.31.26.22 <${app-path}/schema/${component}.js &>>${log_file}
 }
+
+
+maven () {
+
+  echo -e "${color}Install maven ${nocolor}"
+  yum install maven -y &>>$log_file
+
+  app_presetup
+
+
+  echo -e "${color}Download maven dependencies ${nocolor}"
+  mvn clean package &>>$log_file
+  mv target/${component}-1.0.jar ${component}.jar &>>$log_file
+
+  echo -e "${color}Install mysql client ${nocolor}"
+  yum install mysql -y
+
+  echo -e "${color}Load schema ${nocolor}"
+  mysql -h 172.31.26.201 -uroot -pRoboShop@1 < $app_path/schema/${component}.sql &>>$log_file
+
+  echo -e "${color}${nocolor}"
+  cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>/tmp/robosho.log
+
+   systemd_setup
+  }
--- shipping.sh
+++ shipping.sh
@@ -1,33 +1,5 @@
-echo -e "\e[34m Install maven \e[0m"
-yum install maven -y &>>/tmp/roboshop.log
+source common.sh
+component=shipping
 
-echo -e "\e[32m Add application user \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
+maven
 
-echo-e "\e[32m Create app directory \e[0m"
-mkdir /app &>>/tmp/roboshop.log
-
-echo -e "\e[33m Download application content \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-cd /app
-
-echo -e "\e[32m Extract the application content \e[0m"
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-cd /app
-
-echo -e "\e[33m Download maven dependencies \e[0m"
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-echo -e "\e[33m Install mysql client \e[0m"
-yum install mysql -y
-
-echo -e "\e[34m Load schema \e[0m"
-mysql -h 172.31.26.201 -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-
-echo -e "\e[32m \e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/robosho.log
-echo -e "\e[32m Start shipping service \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl start shipping &>>/tmp/roboshop.log
