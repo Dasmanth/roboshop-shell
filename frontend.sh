9cbba3c7b4920b4ebeb4aaa9731269cb8e965813
Dasmanth <dasmanthkare@gmail.com>
2023-10-23 12:53:43 +0530

    Frontend changes

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "\e[31mInstalling nginx server\e[0m"
+echo -e "${color}Installing nginx server${nocolor}"
 yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "\e[31mRemoving old app content\e[0m"
+echo -e "${color}Removing old app content${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>/tmp/robshop.log
 
-echo -e "\e[31mDownloading frontend content\e[0m"
+echo -e "${color}Downloading frontend content${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[31mExtracting the frontend content\e[0m"
+echo -e "${color}Extracting the frontend content${nocolor}"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "\e[33mupdate frontend configuration\e[0m"
+echo -e "${color}mupdate frontend configuration${nocolor}"
 cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[33mStart nginx server\e[0m"
+echo -e "${color}mStart nginx server${nocolor}"
 systemctl enable nginx &>>/tmp/roboshop.log
 systemctl restart nginx &>>/tmp/roboshop.log
