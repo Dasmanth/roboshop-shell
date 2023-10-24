c77d41ebfc1c5faf828027338220a0e7081fe3e1
Dasmanth <dasmanthkare@gmail.com>
2023-10-24 12:25:48 +0530

    S variables

--- 03.vars.sh
+++ 03.vars.sh
@@ -4,3 +4,12 @@ echo value of a is $adollars
 
 # Alternatively variables also can be accessed by using ${}
 echo value 0f a is ${a}dollars
+
+
+# special variables for inputs
+# special variables used for inputs are $0 to $n, $*, $#
+
+echo $0
+echo $1
+echo $*
+echo $#
