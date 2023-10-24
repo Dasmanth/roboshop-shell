bc285f98e08ea7119d90fb1107ac3f16266192e7
Dasmanth <dasmanthkare@gmail.com>
2023-10-24 12:35:16 +0530

    S functions

--- 04.functions.sh
+++ 04.functions.sh
@@ -3,3 +3,12 @@ print_hello() {
 }
 
 print_hello
+
+print_arguments() {
+  echo first argument $1
+  echo second argument $2
+  echo all arguments - $*
+  echo no of arguments - $#
+}
+
+prnit_arguments abc 123 xyz
